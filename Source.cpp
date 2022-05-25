4c0881396d10060c549535b3c76dc48b277f8099
Navai Suleymanli <80199998+Navai-Suleymanli@users.noreply.github.com>
2022-05-25 19:01:27 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b, c;
+
+    // Get inputs
+    cin >> a >> b >> c;
+
+    // Print their sum to the screen
+    cout << a + b + c << "\n";
+}
